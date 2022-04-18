15f40d58cea88ef592c4b54c6fb05fe890f340cb
Ruan Pletsch <ruanpletsch@gmail.com>
2022-04-18 18:17:10 -0300

    created all functions, missed tests

--- src/index.js
+++ src/index.js
@@ -10,32 +10,111 @@ app.use(express.json());
 
 const users = [];
 
+//Middleware
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers
+
+  const user = users.find(user => user.username === username)
+
+  if(!user){
+    return response.status(404).json({error: 'User do not exist'})
+  }
+
+  request.user = user
+
+  return next();
 }
 
+//Create user
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const {name, username} = request.body
+
+  const usernameAlreadyUsed = users.find(user => user.username === username)
+
+  if(usernameAlreadyUsed){
+    return response.status(400).json({error: 'Username already claimed!'})
+  }
+
+  const user = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: []
+    }
+
+  users.push(user)
+
+  return response.status(201).json(user)
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {user} = request
+
+  return response.json(users.todos)
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {user} = request
+  const {title, deadline} = request.body
+
+  const todo = ({
+      id: uuidv4(),
+      title,
+      done: false,
+      deadLine: new Date(deadline),
+      created_at: new Date()
+  })
+
+  user.todos.push(todo)
+
+  return response.status(201).json(todo)
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {user} = request
+  const {title, deadLine}= request.body
+  const {id} = request.params
+
+  const validTodo = user.todos.find(todo => todo.id === id)
+
+  if (!validTodo){
+    return response.status(404).json({error: 'Todo invalid'})
+  }
+
+  todo.title = title
+  todo.deadLine = new Date(deadLine)
+
+  return response.json(validTodo)
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {user} = request
+  const {id} = request.params
+
+  const validTodo = user.todos.find(todo => todo.id === id)
+
+  if (!validTodo){
+    return response.status(404).json({error: 'Todo invalid'})
+  }
+
+  validTodo.done = true
+
+  return response.json(validTodo)
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {user} = request
+  const {id} = request.params
+
+  const todoSpot = user.todos.findIndex(todo => todo.id === id) 
+
+  if (todoSpot === -1){
+    return response.status(404).json({error: 'Todo invalid'})
+  }
+
+  user.todos.splice(todoSpot, 1)
+
+  return request.status(204).json()
 });
 
 module.exports = app;
